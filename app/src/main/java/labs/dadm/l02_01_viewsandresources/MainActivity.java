858441b26b9f4e94c16a4229b81329d697181953
ddandres <ddandres@disca.upv.es>
2019-11-26 16:31:24 +0100

    Updated to AndroidX

--- app/src/main/java/labs/dadm/l02_01_viewsandresources/MainActivity.java
+++ app/src/main/java/labs/dadm/l02_01_viewsandresources/MainActivity.java
@@ -5,7 +5,7 @@
 package labs.dadm.l02_01_viewsandresources;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
